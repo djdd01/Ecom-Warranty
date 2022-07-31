ad412d0446135ff25b8cdf27159b302ab3f030c0
DJDD01 <dhruv01dubey@gmail.com>
2022-07-31 14:38:54 +0530

    initial commit

---
+++ packages/react-app/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { PageHeader } from "antd";
+import React from "react";
+
+// displays a page header
+
+export default function Header() {
+    return (
+        <PageHeader
+            title=""
+            subTitle=""
+            style={{ cursor: "pointer" }}
+        />
+    );
+}
